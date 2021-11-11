eb0cf77ca6fc91ddc69c9d835ea4036d3cfad89c
ZeynepAkyokus <56530792+ZeynepAkyokus@users.noreply.github.com>
2021-11-12 02:49:35 +0300

    Initial Commit

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return  args -> {
+            Student zeynep = new Student( "Zeynep", "zeynep@gmail.com",
+                    LocalDate.of(1998, 6, 26));
+
+            Student ahmet = new Student("Ahmet", "ahmet@gmail.com",
+                    LocalDate.of(2002, 6, 26));
+
+            repository.saveAll(List.of(zeynep,ahmet));
+        };
+    }
+}
